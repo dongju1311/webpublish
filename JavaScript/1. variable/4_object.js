bfe0813b46838e1f36448487d6bf4f9301f9a663
dongju1311 <ghkd1311@naver.com>
2025-08-19 15:02:28 +0900

    25/08/19 commit

---
+++ JavaScript/1. variable/4_object.js
@@ -0,0 +1,42 @@
+/**
+ * 객체형
+ * Class, Array([]), Object({})
+ * Array 형식 : [데이터1, 데이터2 ...]
+ * Object 형식 : {
+ *  프로퍼티: 데이터,
+ *  프로퍼티: 데이터,
+ *  프로퍼티: 데이터,
+ * ...
+ * }
+ */
+
+let apple = null;
+apple = {
+    // 'name' : 'apple',
+    // 'color' : 'red',
+    // 'emoji' : '🍎'
+    name : 'apple',
+    color : 'red',
+    emoji : '🍎'
+};
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.emoji);
+
+//orange 객체 정의 및 생성
+let orange = {
+    name  : 'orange',
+    color : "coral",
+    emoji : '🍊'
+};
+
+//Array
+let numbers = [1,2,3,4,5];
+console.log(numbers[0]);
+console.log(numbers[3]);
+console.log(numbers);
+
+let fruits = [apple, orange]; //위에 apple과 orange가 선언되어야 가능함
+console.log(fruits);
+console.log(fruits[0].emoji);
+console.log(fruits[1].emoji);
