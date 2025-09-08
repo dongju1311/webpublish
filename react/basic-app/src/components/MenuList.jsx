84776a1a1345a839533748b72bdfc4c4ae103ff7
dongju1311 <ghkd1311@naver.com>
2025-09-08 17:08:50 +0900

    25/09/08 commit

---
+++ react/basic-app/src/components/MenuList.jsx
@@ -0,0 +1,16 @@
+/**
+ * 메뉴 리스트
+ */
+import { Menu } from "./Menu.jsx";
+
+export function MenuList({menus}) {
+    return(
+        <ul className="menu-list">
+            {menus.map((menu)=>
+            <li>
+                <Menu title={menu.title} color={menu.color} bg={menu.bg} href={menu.href}/>
+            </li>
+            )}
+        </ul>
+    );
+}
