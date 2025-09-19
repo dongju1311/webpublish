b9ac62f166bb9c0dbf68134ac4ffc97deec9561c
dongju1311 <ghkd1311@naver.com>
2025-09-19 16:51:09 +0900

    25/09/19 commit

--- react/shoppy-app/src/components/commons/Header.jsx
+++ react/shoppy-app/src/components/commons/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
+import { GiShoppingCart } from 'react-icons/gi';
 
-export function Header() {
+
+export function Header({cartCount}) {
+
     return(
         <div className="header-outer">
             <div className="header">
@@ -11,6 +14,10 @@ export function Header() {
                 </Link>
                 <nav className="header-right">
                     <Link to="/all">Products</Link>
+                    <Link to="/cart" className="header-icons-cart-link">
+                        <GiShoppingCart className='header-icons'/>
+                        <span className="header-icons-cart">{cartCount}</span>
+                    </Link>
                     <Link to="/login">
                         <button type="button">Login</button>
                     </Link>
--- react/shoppy-app/src/components/product/ProductList.jsx
+++ react/shoppy-app/src/components/product/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ProductAvatar } from './ProductAvatar';
 import { fetchData, axiosData, groupByRows } from '../../utils/dataFetch.js';
 
@@ -19,7 +20,9 @@ export function ProductList() {
             {rows && rows.map((rowArray,idx)=>
                 <div className='product-list' key={idx}>
                     {rowArray && rowArray.map((product,idx)=>
-                        <ProductAvatar img={product.image} key={idx}/>
+                        <Link to={`/products/${product.pid}`}>
+                            <ProductAvatar img={product.image} key={idx}/>
+                        </Link>
                     )}
                 </div>
             )}
