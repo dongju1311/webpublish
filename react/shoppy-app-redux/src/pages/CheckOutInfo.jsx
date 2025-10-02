a674bbb6bc9abf49b775eec7d01afd1695d24de5
dongju1311 <ghkd1311@naver.com>
2025-10-02 11:52:10 +0900

    25/10/02 commit

--- react/shoppy-app-redux/src/pages/CheckOutInfo.jsx
+++ react/shoppy-app-redux/src/pages/CheckOutInfo.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useContext } from "react";
-import { useLocation } from "react-router-dom";
-import { CartContext } from "../context/CartContext.js";
+import { useSelector} from 'react-redux';
 import "../styles/cart.css";
 import "../styles/checkoutinfo.css";
 
 export function CheckoutInfo() {   
-    const {cartList, totalPrice} = useContext(CartContext);
+    const cartList = useSelector((state)=>state.cart.cartList);
+    const totalPrice = useSelector((state)=>state.cart.totalPrice);
 return (
     <div className="cart-container">
     <h2 className="cart-header"> 주문/결제</h2>
